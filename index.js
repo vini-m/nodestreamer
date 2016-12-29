54609673a6c2a503f1289fb295f1ee97c4f2297f
Vini M <vinicyusmacedo@outlook.com>
2016-12-29 19:47:50 -0200

    Added Materialize, changed index template (WIP) and added cluster to keep server stable

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ var express_port = process.env.PORT || 3000;
 var express_ip = '0.0.0.0';
 app.set('port', express_port);
 
+// Setting up Cluster
+var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
+
 // Pug template engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -24,13 +28,30 @@ app.use(
 // Import the router
 app.use(router);
 
-// Create ponyplayer server
-var createServerCallback = function () {
-    console.log('Nodestreamer is listening on port ' + app.get('port'));
-};
-
-http.createServer(app).listen(
-    app.get('port'),
-    express_ip,
-    createServerCallback
-);
+// Snippet taken from https://github.com/licson0729/node-YouTubeStreamer/blob/master/server.js
+// Master process, fork child
+if (cluster.isMaster) {
+    // Fork based on the number of CPUs
+    for (var i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+    
+    // Restart childs when they die
+    cluster.on('exit', function (worker, code) {
+        console.log('Worker ' + worker.process.pid + ' died with code ' + code);
+        cluster.fork();
+    });
+    
+    cluster.on('online', function (worker) {
+        console.log('Worker ' + worker.process.pid + ' is ready!');
+    });
+} else {
+    var createServer = function () {
+        console.log('Nodestreamer is listening on port ' + app.get('port'));
+    };
+    http.createServer(app).listen(
+        app.get('port'),
+        express_ip,
+        createServer
+    );
+}
